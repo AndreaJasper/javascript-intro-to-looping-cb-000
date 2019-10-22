39cd1d622d8d812dc46284f3a9fa958276e7d6e6
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2019-10-22 20:56:58 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,7 +1,6 @@
 for ( var i = 0; i < 25; i++)
-  if {
-    i === 1;
-  return "I am ${i} strange loop.";
+  if (i === 1){
+    return "I am 1 strange loop.";
 }
   else {
     return "I am ${i} strange loops."
